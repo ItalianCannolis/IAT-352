937f764b01bea92800ee855fac1612305979a692
ItalianCannolis <nathan.cerone28@gmail.com>
2023-09-30 17:07:12 -0700

    Starter Code Upload

---
+++ a1_starter_code/a1_starter_code/b_form_functions.php
@@ -0,0 +1,19 @@
+<?php
+
+// I recommend creating the following functions:
+// generateRecipeNameSection()
+// generateIngredientsSection()
+//		this function should create 3 inputs (iquantity, iunit and iname) X 10 times in a loop
+// generateInstructionsSection()
+// create_recipe_form()
+//		this function should call the previous three functions to create a form.
+//		the form should call c_submit_recipe.php when submitted, via a POST request.
+//		also notice this is the only function being called in a_submit_recipe.php
+
+// I'll get you started:
+function create_recipe_form() {
+   
+}
+ // and so on...
+
+?>
